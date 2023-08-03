c8d0fb7e7437e2e8ec99e22ca438720e62936ef1
unknown <gwenmouv26@gmail.com>
2023-08-03 19:48:56 +0200

    :sparkles: feat: Menu déroulant pour la navigation sur petit écran et quelques modifications pour le responsive (Foodie Finds)

--- js/FoodieFinds.js
+++ js/FoodieFinds.js
@@ -1,14 +1,29 @@
-// MENU BURGER
-const toggle = document.getElementById('toggle');
+// MENU DÉROULANT NAVBAR
+const toggleNavbar = document.getElementById('toggle-navbar');
+const navbar = document.getElementById('navbar');
+    document.onclick = function(event) {
+        if(event.target.id !== 'toggle-navbar' && event.target.id !== 'navbar'){
+            toggleNavbar.classList.remove('active');
+            navbar.classList.remove('active');
+        };
+    };
+    toggleNavbar.onclick = function(){
+        toggleNavbar.classList.toggle('active');
+        navbar.classList.toggle('active');
+    };
+
+
+// MENU BURGER CITIES
+const toggleCities = document.getElementById('toggle-cities');
 const citiesNavigation = document.getElementById('FindsNavCities');
         document.onclick = function(event) {
-            if(event.target.id !== 'toggle' && event.target.id !== 'FindsNavCities'){
-                toggle.classList.remove('active');
+            if(event.target.id !== 'toggle-cities' && event.target.id !== 'FindsNavCities'){
+                toggleCities.classList.remove('active');
                 citiesNavigation.classList.remove('active');
             };
         };
-        toggle.onclick = function(){
-            toggle.classList.toggle('active');
+        toggleCities.onclick = function(){
+            toggleCities.classList.toggle('active');
             citiesNavigation.classList.toggle('active');
         };
 
@@ -20,21 +35,24 @@ let activePost = null;
 const findsPostMap = findsPostArray.map(post =>{
     post.addEventListener('click', (event) => {
         event.stopPropagation();
+        if(!activePost && window.innerWidth > 960){
+            post.classList.add('BlownUp');
+            activePost = post;
+            activePost.querySelector('.like').classList.add("active");
+        }
 
-        post.classList.add('BlownUp');
-        activePost = post;
-        activePost.querySelector('.like').classList.add("active");
-
-        const activePostDimensions = activePost.getBoundingClientRect();
-        if(
-            event.clientX < activePostDimensions.left ||
-            event.clientX > activePostDimensions.right ||
-            event.clientY < activePostDimensions.top ||
-            event.clientY > activePostDimensions.bottom
-        ){
-            activePost.classList.remove('BlownUp');
-            activePost.querySelector('.like').classList.remove("active");
-            activePost = null;
+        else if(activePost && window.innerWidth > 960){
+            const activePostDimensions = activePost.getBoundingClientRect();
+            if(
+                event.clientX < activePostDimensions.left ||
+                event.clientX > activePostDimensions.right ||
+                event.clientY < activePostDimensions.top ||
+                event.clientY > activePostDimensions.bottom
+                ){
+                    activePost.classList.remove('BlownUp');
+                    activePost.querySelector('.like').classList.remove("active");
+                    activePost = null;
+            }
         }
     });
 });
